acd74e31265f85d4304fadd196a6d7cebd865577
muhammadkhan-dev-code <muhammadkhansw00@gmail.com>
2025-12-13 14:03:00 +0500

    chapter_2 code

---
+++ chapter_2.py
@@ -0,0 +1,31 @@
+# ----------------data types in python---------------
+# integer
+a = 10
+print(type(a))  # Output: <class 'int'>
+# float
+b = 10.5
+print(type(b))  # Output: <class 'float'>
+# string
+c = "Hello, World!"
+print(type(c))  # Output: <class 'str'>
+# boolean
+d = True
+print(type(d))  # Output: <class 'bool'>
+# list
+e = [1, 2, 3, 4, 5]
+print(type(e))  # Output: <class 'list'>
+# tuple
+f = (1, 2, 3, 4, 5)
+print(type(f))  # Output: <class 'tuple'>
+# dictionary
+g = {"name": "Alice", "age": 25}
+print(type(g))  # Output: <class 'dict'>
+# set
+h = {1, 2, 3, 4, 5}
+print(type(h))  # Output: <class 'set'>
+# NoneType
+i = None
+print(type(i))  # Output: <class 'NoneType'>   
+  
+# -----------------keywords in python -------------------
+#reserved words that have special meaning in Python and cannot be used as identifiers (variable names, function names, etc.)
---
+++ practiceQuestions.py
@@ -0,0 +1,4 @@
+# take age as input and print age and their type
+age= int(input("Enter your age: "))
+print("Your age is: ", age)
+print("Type of age variable is: ", type(age)) 
