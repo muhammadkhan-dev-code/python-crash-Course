d7971605c42e692f4c89863fd8fd3deb5ff192ea
muhammadkhan-dev-code <muhammadkhansw00@gmail.com>
2025-12-13 13:50:12 +0500

    python crashCourse complete code

---
+++ Assignments.py
@@ -0,0 +1,11 @@
+# Assignment_1
+for i in range(1,11):
+    print(i, " Muhammad Khan")
+
+# Assignment_2
+diameter= float(input("Enter the value for diameter of circle: "))
+radius = float(diameter)/2
+area= 3.14 * radius * radius
+print("Area of circle with diameter ", diameter, " is: ", area) 
+
+# Assignment_3
---
+++ chapter_1.py
@@ -0,0 +1,43 @@
+#code execution will be line by line  
+print(" line 1")
+print(" line 2")
+
+
+#  variables stored in memory 
+a= 10;  # var a store the value 10  values can be changed throughout program
+b = "Muhammad Khan"
+print("value of a ",a)
+print("value of b ",b)
+#  name of variable should be  meaningful
+#  variable name should not start with number or special character
+#  variable name should not be python keywords
+#  variable name should not contain spaces
+print (1+5)
+
+#  input function is used to take input from user
+input_name= input ("enter your name: ")
+print("hello ", input_name)
+
+#  # is used for comment 
+#  indentation is important in python
+# python uses indentation to define blocks of code
+if a>5:
+    print("a is greater than 5")
+else:
+    print("a is less than or equal to 5")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
